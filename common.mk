3971826ba395a66759a81f87a7c2c0899841a2f7
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    galaxys2-common: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- common.mk
+++ common.mk
@@ -20,6 +20,9 @@ DEVICE_PACKAGE_OVERLAYS := \
     $(COMMON_PATH)/overlay \
     $(COMMON_PATH)/overlay-lineage
 
+# RRO (Runtime Resource Overlay)
+PRODUCT_ENFORCE_RRO_TARGETS += *
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 # Component overrides
