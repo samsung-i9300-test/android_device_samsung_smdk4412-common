f3bddfcc106bcccee47b68235e65410c5c0d76fe
Dominggoes Isakh <drjisakh@gmail.com>
2021-01-01 16:57:10 +0100

    galaxys2-common: Add vibrator service

--- common.mk
+++ common.mk
@@ -139,7 +139,8 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 # Keymaster
 PRODUCT_PACKAGES += \
