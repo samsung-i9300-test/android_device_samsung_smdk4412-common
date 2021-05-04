9e0eef87c9a1e308398208d9e00b84578242abec
Dominggoes Isakh <drjisakh@gmail.com>
2021-05-04 07:52:39 +0200

    smdk4412-common: Customize GO configuration

--- common.mk
+++ common.mk
@@ -302,8 +302,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-threads=1 \
     dalvik.vm.image-dex2oat-threads=1
 
-# Build with specific settings for Galaxys2-common
-$(call inherit-product, $(LOCAL_PATH)/go_galaxys2-common.mk)
+# Use go
+$(call inherit-product, $(LOCAL_PATH)/go_defaults.mk)
 
 # Apply Dalvik config for 1G phone
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
