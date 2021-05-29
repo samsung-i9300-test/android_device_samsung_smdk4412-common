a82795d18d7ffefc0e9395cf3701861ade8f00ff
Dominggoes Isakh <drjisakh@gmail.com>
2021-05-29 21:45:48 +0200

    galaxys2-common: Reduce GC frequency of foreground apps by 50%

--- common.mk
+++ common.mk
@@ -213,6 +213,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.sys.fw.trim_cache_percent=100 \
     ro.vendor.qti.sys.fw.empty_app_percent=25
 
+# Reduces GC frequency of foreground apps by 50%
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.foreground-heap-growth-multiplier=2.0
+
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     make_ext4fs \
