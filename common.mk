0704a93f63e055aa07d77384c32632fa974ceec8
Dominggoes Isakh <drjisakh@gmail.com>
2021-07-13 17:48:21 +0200

    galaxys2-common: Exclude hardware overlays from RRO

--- common.mk
+++ common.mk
@@ -21,7 +21,8 @@ DEVICE_PACKAGE_OVERLAYS := $(COMMON_PATH)/overlay
 # RRO (Runtime Resource Overlay)
 PRODUCT_ENFORCE_RRO_TARGETS += *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS  += * \
-    $(COMMON_PATH)/overlay-lineage/lineage-sdk
+    $(COMMON_PATH)/overlay-lineage/lineage-sdk \
+    $(COMMON_PATH)/overlay/hardware
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
