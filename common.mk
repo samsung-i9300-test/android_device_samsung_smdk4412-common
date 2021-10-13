0643570e8e3ca380744ca01cccb36bf4a3ca826b
Shilin Victor <chrono.monochrome@gmail.com>
2021-10-13 19:30:30 +0300

    smdk4412-common: disable Lineage Trust build for S bringup

--- common.mk
+++ common.mk
@@ -59,8 +59,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.adb.nonblocking_ffs=false
 
 # Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
+#PRODUCT_PACKAGES += \
+#    vendor.lineage.trust@1.0-service
 
 # Battery
 PRODUCT_PROPERTY_OVERRIDES += \
