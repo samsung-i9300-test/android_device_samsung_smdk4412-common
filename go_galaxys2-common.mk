10c144e5057da2ebb069e1176d5dd783fa44fc69
Shilin Victor <chrono.monochrome@gmail.com>
2021-10-13 19:32:45 +0300

    smdk4412-common: don't use boot image profile for S bringup

--- go_galaxys2-common.mk
+++ go_galaxys2-common.mk
@@ -39,8 +39,8 @@ PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 
 # Use a profile based boot image for this device. Note that this is currently a
 # generic profile and not Android Go optimized.
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+#PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+#PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
 # set the compiler filter for shared apks to quicken.
 # Rationale: speed has a lot of dex code expansion, it uses more ram and space
