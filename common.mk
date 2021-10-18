d475520f2644078e0e330161e033f76b98fb0f4f
Shilin Victor <chrono.monochrome@gmail.com>
2021-10-18 20:42:53 +0300

    smdk4412-common: add ro.config.max_starting_bg=2

--- common.mk
+++ common.mk
@@ -223,7 +223,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.sys.fw.use_trim_settings=true \
     ro.vendor.qti.sys.fw.trim_empty_percent=50 \
     ro.vendor.qti.sys.fw.trim_cache_percent=100 \
-    ro.vendor.qti.sys.fw.empty_app_percent=25
+    ro.vendor.qti.sys.fw.empty_app_percent=25 \
+    ro.config.max_starting_bg=2
 
 # Reduces GC frequency of foreground apps by 50%
 PRODUCT_PROPERTY_OVERRIDES += \
