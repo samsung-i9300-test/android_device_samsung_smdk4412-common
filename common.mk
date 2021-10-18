062c06aaaa4e546454a91c098f5866c4f2e8dc2c
Shilin Victor <chrono.monochrome@gmail.com>
2021-10-18 19:37:35 +0300

    Revert "galaxys2-common: hwcomposer: Binderize"
    This reverts commit 140f8dffe29cceffa15305d80e212661a47a0c9f.

--- common.mk
+++ common.mk
@@ -123,7 +123,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl-exynos4 \
     android.hardware.graphics.mapper@2.0-impl-exynos4 \
     android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.composer@2.1-service \
     hwcomposer.exynos4 \
     gralloc.exynos4 \
     libUMP \
